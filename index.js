6b420ca00b4649c7000ccd40dc02494a7506d821
Anton <erikssonanton94@gmail.com>
2026-02-10 17:28:52 +0100

    Skapat CRUD funktion för att POSTA nya todos. Har även lagt till valideringar av titel och status.

--- index.js
+++ index.js
@@ -18,6 +18,40 @@ app.get('/api/todos', (req, res) => {
 });
 
 // CREATE
+app.post('/api/todos', (req, res) => {
+    const { title, description, status } = req.body;
+
+    //Validerar titeln
+    if (!title || title.length < 3) {
+        return res.status(400).json({ error: "Titel måste vara minst 3 tecken lång." });
+    }
+
+    //Validering av beskrivning (max 200 tecken)
+    if (description && description.length > 200) {
+        return res.status(400).json({ error: "Beskrivningen får max vara 200 tecken." });
+    }
+
+    //Validering av status
+    const allowedStatus = [0, 1, 2]; // 0=Ej påbörjad, 1=Pågående, 2=Avklarad
+    let todoStatus = status !== undefined ? status : 0; // default 0
+    if (!allowedStatus.includes(todoStatus)) {
+        return res.status(400).json({ error: "Status måste vara 0, 1 eller 2." });
+    }
+
+    //Skapar todo
+    const todo = {
+        id: nextId++,
+        title,
+        description: description || "",
+        status: todoStatus
+    };
+
+    //Lägger in ny todo i arrayen todos
+    todos.push(todo);
+    res.status(201).json(todo);
+});
+
+
 
 // UPDATE status
 
